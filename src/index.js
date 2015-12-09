eb0bd82acfc9fcc46cc405fa94a360b5071b08ff
Michael Symmes <msymmes@gmail.com>
2015-12-09 17:48:03 -0500

    Corrected sidebar removal so settings still works.

--- src/index.js
+++ src/index.js
@@ -1,8 +1,26 @@
 var header = document.getElementsByTagName('header');
+//header.style.visibility = 'hidden';
 document.body.removeChild(header[0]);
 
 var footer = document.getElementsByTagName('footer');
+//footer.style.visibility = 'hidden';
 document.body.removeChild(footer[0]);
 
-var sidebar = document.getElementById('sidebar');
-document.getElementById('marketview').removeChild(sidebar);
+var sidebar = document.getElementById('sidebar__cols');
+//sidebar.style.visibility = 'hidden';
+sidebar.parentElement.removeChild(sidebar);
+
+/*
+var listeners;
+var marketOptions = document.getElementById('market');
+listeners = chrome.getEventListeners();
+marketOptions.addEventListener('click', listeners.mousemove);
+listeners.mousemove = function(){};
+
+
+var interval = document.getElementById('interval');
+listeners = chrome.getEventListeners();
+interval.addEventListener('click', listeners.mousemove);
+listeners.mousemove = function(){};
+*/
+
