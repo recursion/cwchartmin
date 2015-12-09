636cc9ba9874fa0c8750457b642828d781db9e91
Michael Symmes <msymmes@gmail.com>
2015-12-09 14:21:53 -0500

    Initial commit.

---
+++ src/index.js
@@ -0,0 +1,8 @@
+var header = document.getElementsByTagName('header');
+document.body.removeChild(header[0]);
+
+var footer = document.getElementsByTagName('footer');
+document.body.removeChild(footer[0]);
+
+var sidebar = document.getElementById('sidebar');
+document.getElementById('marketview').removeChild(sidebar);
