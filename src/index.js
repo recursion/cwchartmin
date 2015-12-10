49a0493df8a451ca22a04a4cfdd156a834a0003d
Michael Symmes <msymmes@gmail.com>
2015-12-09 20:54:46 -0500

    Fixed some issues.

--- src/index.js
+++ src/index.js
@@ -10,13 +10,10 @@ var sidebar = document.getElementById('sidebar__cols');
 //sidebar.style.visibility = 'hidden';
 sidebar.parentElement.removeChild(sidebar);
 
-/*
-var listeners;
 var marketOptions = document.getElementById('market');
-listeners = chrome.getEventListeners();
-marketOptions.addEventListener('click', listeners.mousemove);
-listeners.mousemove = function(){};
+marketOptions.mousemove = function(){};
 
+/*
 
 var interval = document.getElementById('interval');
 listeners = chrome.getEventListeners();
